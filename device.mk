2be546c91c2eff728870497e26a854afcf4c24e1
Lucas Dupin <dupin@google.com>
2021-06-03 16:45:21 -0700

    Munch: Enable blurs
    Bug: 181092919
    Test: manual; performance scripts
    Change-Id: I5875a99167e80ddd5b79fc245f988d8a6ac167be

--- device.mk
+++ device.mk
@@ -165,6 +165,7 @@ PRODUCT_VENDOR_PROPERTIES += \
     ro.surface_flinger.set_idle_timer_ms=1000 \
     ro.surface_flinger.set_touch_timer_ms=1000 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
+    ro.surface_flinger.supports_background_blur=1 \
     ro.surface_flinger.use_content_detection_for_refresh_rate=true \
     ro.vendor.display.paneltype=2 \
     ro.vendor.display.sensortype=2 \
