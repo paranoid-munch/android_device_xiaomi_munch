d19c3b6ea14c33e6a26f963e8ff4dafdc1c3f1d0
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-24 11:54:42 +0200

    Munch: libinit: Import changes from sm8150-common
    Change-Id: Ia70da22bb9dd49528a4e35b49076bf5647d5e7be

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,6 +62,10 @@ DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/configs/hidl/manifest.xml \
     $(DEVICE_PATH)/configs/hidl/xiaomi_manifest.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB ?= //$(DEVICE_PATH):init_xiaomi_kona
+TARGET_RECOVERY_DEVICE_MODULES ?= init_xiaomi_kona
+
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 3
 BOARD_KERNEL_BASE := 0x00000000
