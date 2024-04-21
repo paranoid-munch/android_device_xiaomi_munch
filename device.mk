12c4e228403e759877f03d70e031ddde38950aff
Anubhav Ruhela <quic_aruhela@quicinc.com>
2024-04-21 23:01:48 +0000

    munch: Switch to AIDL QTI Boot control service
    Change-Id: I5d2663afdbdb21905d66fb6903f81c5edf012b99

--- device.mk
+++ device.mk
@@ -111,9 +111,8 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Boot
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service
+    android.hardware.boot-service.qti \
+    android.hardware.boot-service.qti.recovery
 
 # Camera
 PRODUCT_PACKAGES += \
