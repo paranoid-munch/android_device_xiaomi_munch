1e4ca714d973753fdf8db3197ade0ae8d96b206e
kaffuchino233 <qkivan@proton.me>
2025-11-07 17:30:29 +0800

    Munch: Switch to common QTI WFD component
    Change-Id: Ifda1468aca61188da3906d65fcaf9e727bf9e24a

--- device.mk
+++ device.mk
@@ -281,6 +281,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     overlay \
     perf \
     telephony \
+    wfd \
     wlan
 
 # QTI fwk-detect
@@ -353,10 +354,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     firmware_WCNSS_qcom_cfg.ini_symlink \
     firmware_wlan_mac.bin_symlink
-
-# Wi-Fi Display
-PRODUCT_PACKAGES += \
-    libwfdaac_vendor
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/munch/munch-vendor.mk)
