f6cc624e0a3efaf753e8b5bd13d7c233e317a34e
HDzungx <hdzungx@gmail.com>
2026-01-20 03:16:20 +0700

    munch: parts: Import DC Dimming and HBM Settings
    * Implement DC Dimming preference
    * Disable DC Dimming if it's not supported
    * Implement HBM switch
    * Adapt HBM and DC dimming settings to S style
    * Define DC dimming and HBM nodes / keys in one place
    * Declare exported flags in manifest
    * Fix compilation for QPR1
    * Fix compilation for Android 15
    * Restore brightness after HBM usage
    * Move to SwitchPreferenceCompat
    * Adapt to munch
    * Convert for M3E support
    
    Co-Authored-By: Nauval Rizky <enuma.alrizky@gmail.com>
    Co-Authored-By: Adhitya Mohan <me@adhityamohan.in>
    Co-Authored-By: TheScarastic <warabhishek@gmail.com>
    Co-Authored-By: alibei <sebastiankotzias83@gmail.com>
    Co-Authored-By: kamikaonashi <thorschi93@gmx.at>
    Signed-off-by: HDzungx <hdzungx@gmail.com>
    Signed-off-by: olzhas0986 <kdiroljas@gmail.com>

---
+++ parts/src/org/lineageos/settings/hbm/HBMFragment.java
@@ -0,0 +1,142 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+* Copyright (C) 2018-2021 crDroid Android Project
+* Copyright (C) 2019-2022 Evolution X Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package org.lineageos.settings.hbm;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceManager;
+import androidx.preference.TwoStatePreference;
+import com.android.settingslib.widget.SettingsBasePreferenceFragment;
+import org.lineageos.settings.utils.FileUtils;
+import org.lineageos.settings.R;
+
+public class HBMFragment extends SettingsBasePreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+
+    public static final String HBM_SWITCH_KEY = "hbm";
+    public static final String AUTO_HBM_SWITCH_KEY = "auto_hbm";
+    public static final String AUTO_HBM_THRESHOLD_KEY = "auto_hbm_threshold";
+    public static final String HBM_DISABLE_TIME_KEY = "hbm_disable_time";
+    public static final String HBM_TURN_OFF_ON_SCREEN_OFF_KEY = "hbm_turn_off_on_screen_off";
+
+    private TwoStatePreference mHBMModeSwitch;
+    private TwoStatePreference mAutoHBMSwitch;
+    private TwoStatePreference mHBMScreenOffSwitch;
+
+    private final SharedPreferences.OnSharedPreferenceChangeListener mPrefsListener =
+            (prefs, key) -> {
+                switch (key) {
+                    case HBM_SWITCH_KEY:
+                        if (mHBMModeSwitch != null) {
+                            mHBMModeSwitch.setChecked(prefs.getBoolean(HBM_SWITCH_KEY, false));
+                        }
+                        if (mHBMScreenOffSwitch != null) {
+                            mHBMScreenOffSwitch.setEnabled(prefs.getBoolean(HBM_SWITCH_KEY, false));
+                        }
+                        break;
+                    case AUTO_HBM_SWITCH_KEY:
+                        if (mAutoHBMSwitch != null) {
+                            mAutoHBMSwitch.setChecked(prefs.getBoolean(AUTO_HBM_SWITCH_KEY, false));
+                        }
+                        break;
+                }
+            };
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this.getContext());
+        setPreferencesFromResource(R.xml.hbm_settings, rootKey);
+
+        // HBM
+        mHBMModeSwitch = findPreference(HBM_SWITCH_KEY);
+        if (mHBMModeSwitch != null) {
+            mHBMModeSwitch.setOnPreferenceChangeListener(new HBMModeSwitch(getContext()));
+        }
+
+        // AutoHBM
+        mAutoHBMSwitch = findPreference(AUTO_HBM_SWITCH_KEY);
+        if (mAutoHBMSwitch != null) {
+            mAutoHBMSwitch.setOnPreferenceChangeListener(this);
+            mAutoHBMSwitch.setChecked(prefs.getBoolean(AUTO_HBM_SWITCH_KEY, false));
+        }
+
+        // HBM turn off on screen off
+        mHBMScreenOffSwitch = findPreference(HBM_TURN_OFF_ON_SCREEN_OFF_KEY);
+        if (mHBMScreenOffSwitch != null) {
+            mHBMScreenOffSwitch.setChecked(prefs.getBoolean(HBM_TURN_OFF_ON_SCREEN_OFF_KEY, false));
+            mHBMScreenOffSwitch.setOnPreferenceChangeListener(this);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+        prefs.registerOnSharedPreferenceChangeListener(mPrefsListener);
+        if (mHBMModeSwitch != null) {
+            mHBMModeSwitch.setChecked(prefs.getBoolean(HBM_SWITCH_KEY, false));
+        }
+        if (mAutoHBMSwitch != null) {
+            mAutoHBMSwitch.setChecked(prefs.getBoolean(AUTO_HBM_SWITCH_KEY, false));
+        }
+        if (mHBMScreenOffSwitch != null) {
+            mHBMScreenOffSwitch.setChecked(prefs.getBoolean(HBM_TURN_OFF_ON_SCREEN_OFF_KEY, false));
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (getContext() != null) {
+            PreferenceManager.getDefaultSharedPreferences(getContext())
+                    .unregisterOnSharedPreferenceChangeListener(mPrefsListener);
+        }
+    }
+
+    public static boolean isAUTOHBMEnabled(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(AUTO_HBM_SWITCH_KEY, false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mAutoHBMSwitch) {
+            Boolean enabled = (Boolean) newValue;
+            PreferenceManager.getDefaultSharedPreferences(getContext())
+                    .edit()
+                    .putBoolean(AUTO_HBM_SWITCH_KEY, enabled)
+                    .apply();
+            FileUtils.enableService(getContext());
+            return true;
+        }
+
+        if (preference == mHBMScreenOffSwitch) {
+            Boolean enabled = (Boolean) newValue;
+            PreferenceManager.getDefaultSharedPreferences(getContext())
+                    .edit()
+                    .putBoolean(HBM_TURN_OFF_ON_SCREEN_OFF_KEY, enabled)
+                    .apply();
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ parts/src/org/lineageos/settings/hbm/HBMModeSwitch.java
@@ -0,0 +1,107 @@
+package org.lineageos.settings.hbm;
+
+import android.provider.Settings;
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+import android.widget.Toast;
+import org.lineageos.settings.R;
+import org.lineageos.settings.utils.FileUtils;
+
+public class HBMModeSwitch implements OnPreferenceChangeListener {
+    private static final String DC_DIMMING_KEY = "dc_dimming";
+    private static final String HBM_NODE = "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/hbm";
+    private static final String BACKLIGHT_NODE = "/sys/class/backlight/panel0-backlight/brightness";
+    private final Context mContext;
+
+    public HBMModeSwitch(Context context) {
+        mContext = context;
+    }
+
+    public static String getHBM() {
+        return FileUtils.isFileWritable(HBM_NODE) ? HBM_NODE : null;
+    }
+
+    public static String getBACKLIGHT() {
+        return FileUtils.isFileWritable(BACKLIGHT_NODE) ? BACKLIGHT_NODE : null;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean enabled = (Boolean) newValue;
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
+        boolean dcDimmingEnabled = prefs.getBoolean(DC_DIMMING_KEY, false);
+
+        if (enabled && dcDimmingEnabled) {
+            Toast.makeText(
+                    mContext,
+                    R.string.hbm_disable_dc_dimming_first,
+                    Toast.LENGTH_SHORT
+            ).show();
+            return false;
+        }
+
+        executeHBMAction(mContext, prefs, enabled);
+        return true;
+    }
+
+    public static void executeHBMAction(Context context, SharedPreferences prefs, boolean enabled) {
+        String hbmNode = getHBM();
+        String backlightNode = getBACKLIGHT();
+
+        if (enabled) {
+            int currentBrightness = Settings.System.getInt(
+                    context.getContentResolver(),
+                    Settings.System.SCREEN_BRIGHTNESS,
+                    128
+            );
+            int currentBrightnessMode = Settings.System.getInt(
+                    context.getContentResolver(),
+                    Settings.System.SCREEN_BRIGHTNESS_MODE,
+                    Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL
+            );
+            
+            prefs.edit()
+                    .putInt("last_brightness", currentBrightness)
+                    .putInt("last_brightness_mode", currentBrightnessMode)
+                    .apply();
+
+            Settings.System.putInt(
+                    context.getContentResolver(),
+                    Settings.System.SCREEN_BRIGHTNESS_MODE,
+                    Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL
+            );
+
+            if (hbmNode != null) FileUtils.writeLine(hbmNode, "1");
+            if (backlightNode != null) FileUtils.writeLine(backlightNode, "2047");
+            
+            Settings.System.putInt(
+                    context.getContentResolver(),
+                    Settings.System.SCREEN_BRIGHTNESS,
+                    255
+            );
+        } else {
+            if (hbmNode != null) FileUtils.writeLine(hbmNode, "0");
+
+            int lastBrightness = prefs.getInt("last_brightness", 128);
+            if (backlightNode != null) FileUtils.writeLine(backlightNode, String.valueOf(lastBrightness));
+            
+            Settings.System.putInt(
+                    context.getContentResolver(),
+                    Settings.System.SCREEN_BRIGHTNESS,
+                    lastBrightness
+            );
+
+            int lastBrightnessMode = prefs.getInt("last_brightness_mode",
+                    Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+            Settings.System.putInt(
+                    context.getContentResolver(),
+                    Settings.System.SCREEN_BRIGHTNESS_MODE,
+                    lastBrightnessMode
+            );
+        }
+    }
+}
