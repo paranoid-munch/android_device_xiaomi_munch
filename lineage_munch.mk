c75212a3ba9a113bc88bdd5a8c16e91d972f5546
olzhas0986 <kdiroljas@gmail.com>
2026-07-23 12:40:09 +0500

    munch: Update blobs to OS1.0.12.0.ULMMIXM
    Signed-off-by: olzhas0986 <kdiroljas@gmail.com>

--- lineage_munch.mk
+++ lineage_munch.mk
@@ -23,5 +23,5 @@ PRODUCT_MODEL := POCO F4
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="munch-user 13 RKQ1.211001.001 V816.0.9.0.ULMMIXM release-keys" \
-    BuildFingerprint=POCO/munch_global/munch:13/RKQ1.211001.001/V816.0.9.0.ULMMIXM:user/release-keys
+    BuildDesc="munch-user 13 RKQ1.211001.001 V816.0.12.0.ULMMIXM release-keys" \
+    BuildFingerprint=POCO/munch_global/munch:13/RKQ1.211001.001/V816.0.12.0.ULMMIXM:user/release-keys
