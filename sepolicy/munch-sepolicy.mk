2e188afd7bcdfd46986684d4105f8054634ca66d
olzhas0986 <kdiroljas@gmail.com>
2026-08-02 22:46:06 +0500

    Munch: sepolicy: Connect parts sepolicy

--- sepolicy/munch-sepolicy.mk
+++ sepolicy/munch-sepolicy.mk
@@ -29,6 +29,7 @@ BOARD_VENDOR_SEPOLICY_DIRS += \
     device/xiaomi/munch/sepolicy/vendor/xiaomi/fingerprint \
     device/xiaomi/munch/sepolicy/vendor/xiaomi/ir \
     device/xiaomi/munch/sepolicy/vendor/xiaomi/modem \
+    device/xiaomi/munch/sepolicy/vendor/xiaomi/parts \
     device/xiaomi/munch/sepolicy/vendor/xiaomi/power_supply \
     device/xiaomi/munch/sepolicy/vendor/xiaomi/sensors \
     device/xiaomi/munch/sepolicy/vendor/xiaomi/thermald \
