RisingOS-Revived-devices/device_xiaomi_munch

munch: Migrate libcameraservice ext to soong_config_set
Signed-off-by: Olzhas-Kdyr <kdiroljas@gmail.com>

device.mk
@@ -192,6 +192,9 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
+# Camera
+$(call soong_config_set,libcameraservice,ext_lib,libcameraservice_extension.xiaomi_sm8250)
+
 # Device-specific settings
 PRODUCT_PACKAGES += \
     XiaomiParts

paranoid-munch/android_device_xiaomi_munch

Signed-off-by: olzhas0986 <kdiroljas@gmail.com>

device.mk
@@ -155,6 +155,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
+# Camera
+$(call soong_config_set,libcameraservice,ext_lib,libcameraservice_extension.xiaomi_munch)
+
 # Device-specific settings
 PRODUCT_PACKAGES += \
     XiaomiParts \
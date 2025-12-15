0e9b4c30d89ba733eae6cb48f66a7fe2523e711f
Olzhas-Kdyr <kdiroljas@gmail.com>
2025-12-15 22:39:35 +0500

    munch: Migrate libcameraservice ext to soong_config_set
    Signed-off-by: olzhas0986 <kdiroljas@gmail.com>

--- device.mk
+++ device.mk
@@ -155,6 +155,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
+# Camera
+$(call soong_config_set,libcameraservice,ext_lib,libcameraservice_extension.xiaomi_munch)
+
 # Device-specific settings
 PRODUCT_PACKAGES += \
     XiaomiParts \
