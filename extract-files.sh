41c95d766c9cd2e5ac7f0d86bb2e768b8b97c787
olzhas0986 <kdiroljas@gmail.com>
2026-08-25 12:45:42 +0500

    munch: Rewrite extract-files in bash

---
+++ extract-files.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+DEVICE=munch
+VENDOR=xiaomi
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+ANDROID_ROOT="${MY_DIR}/../../.."
+
+# If XML files don't have comments before the XML header, use this flag
+# Can still be used with broken XML files by using blob_fixup
+export TARGET_DISABLE_XML_FIXING=true
+
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+# Default to sanitizing the vendor folder before extraction
+CLEAN_VENDOR=true
+
+ONLY_FIRMWARE=
+KANG=
+SECTION=
+CARRIER_SKIP_FILES=()
+
+while [ "${#}" -gt 0 ]; do
+    case "${1}" in
+        --only-firmware)
+            ONLY_FIRMWARE=true
+            ;;
+        -n | --no-cleanup)
+            CLEAN_VENDOR=false
+            ;;
+        -k | --kang)
+            KANG="--kang"
+            ;;
+        -s | --section)
+            SECTION="${2}"
+            shift
+            CLEAN_VENDOR=false
+            ;;
+        *)
+            SRC="${1}"
+            ;;
+    esac
+    shift
+done
+
+if [ -z "${SRC}" ]; then
+    SRC="adb"
+fi
+
+function blob_fixup() {
+    case "${1}" in
+        vendor/etc/init/init.batterysecret.rc)
+            [ "$2" = "" ] && return 0
+            sed -i '/seclabel u:r:batterysecret:s0/d' "${2}"
+            ;;
+        vendor/etc/libnfc-nci.conf)
+            [ "$2" = "" ] && return 0
+            grep -q content "LEGACY_MIFARE_READER=1" "${2}" || echo "LEGACY_MIFARE_READER=1" >> "${2}"
+            ;;
+        vendor/lib/libaudioroute_ext.so|vendor/lib/hw/audio.primary.kona.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "libaudioroute.so" "libaudioroute-v34.so" "${2}"
+            ;;
+        vendor/lib64/camera/components/com.mi.node.watermark.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
+            ;;
+        vendor/lib64/hw/camera.qcom.so)
+            [ "$2" = "" ] && return 0
+            sed -i 's|st_license.lic|camera_cnf.txt|g' "${2}"
+            ;;
+        vendor/lib64/libMIAIHDRhvx_interface.so|vendor/lib64/libarcsoft_hdrplus_hvx_stub.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF_0_8}" --clear-symbol-version "remote_handle_close" "${2}"
+            "${PATCHELF_0_8}" --clear-symbol-version "remote_handle_invoke" "${2}"
+            "${PATCHELF_0_8}" --clear-symbol-version "remote_handle_open" "${2}"
+            ;;
+        vendor/lib64/libarcsoft_super_night_raw.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF_0_8}" --clear-symbol-version "rpcmem_alloc" "${2}"
+            "${PATCHELF_0_8}" --clear-symbol-version "rpcmem_free" "${2}"
+            "${PATCHELF_0_8}" --clear-symbol-version "rpcmem_to_fd" "${2}"
+            ;;
+        vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
+            [ "$2" = "" ] && return 0
+            echo "9a0a0094" | xxd -r -p | sed -i 's/\x9a\x0a\x00\x94/\x1f\x20\x03\xd5/g' "${2}"
+            ;;
+        vendor/etc/init/init.mi_thermald.rc)
+            [ "$2" = "" ] && return 0
+            sed -i '/seclabel u:r:mi_thermald:s0/d' "${2}"
+            ;;
+        vendor/etc/seccomp_policy/atfwd@2.0.policy)
+            [ "$2" = "" ] && return 0
+            grep -q "gettid: 1" "${2}" || echo "gettid: 1" >> "${2}"
+            ;;
+        vendor/lib64/libril-qc-hal-qmi.so)
+            [ "$2" = "" ] && return 0
+            sed -i 's|ro.product.vendor.device|ro.vendor.radio.midevice|g' "${2}"
+            ;;
+        vendor/lib64/libwvhidl.so|vendor/lib64/mediadrm/libwvdrmengine.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
+            ;;
+        vendor/lib/libstagefright_soft_ac4dec.so|vendor/lib/libstagefright_soft_ddpdec.so|vendor/lib/libstagefrightdolby.so|vendor/lib64/libdlbdsservice.so|vendor/lib64/libstagefright_soft_ac4dec.so|vendor/lib64/libstagefright_soft_ddpdec.so|vendor/lib64/libstagefrightdolby.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
+}
+
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
+
+if [ -z "${ONLY_FIRMWARE}" ]; then
+    extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+fi
+
+"${MY_DIR}/setup-makefiles.sh"
