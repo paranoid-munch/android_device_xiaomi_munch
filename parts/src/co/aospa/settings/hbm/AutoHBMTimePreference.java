f6d30fe05a7ae3b037af36640110d94a6aa3fd46
olzhas0986 <kdiroljas@gmail.com>
2026-08-29 19:25:42 +0500

    munch: parts: Switch to Paranoid Android namespace

---
+++ parts/src/co/aospa/settings/hbm/AutoHBMTimePreference.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2016 The OmniROM Project
+                 2023 The Evolution X Project
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+package co.aospa.settings.hbm;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.AttributeSet;
+import androidx.preference.PreferenceManager;
+
+public class AutoHBMTimePreference extends CustomSeekBarPreference {
+
+    private static final int mMinVal = 1;
+    private static final int mMaxVal = 10;
+    private static final int mDefVal = 1;
+
+    public AutoHBMTimePreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mInterval = 1;
+        mShowSign = false;
+        mUnits = "";
+        mContinuousUpdates = false;
+        mMinValue = mMinVal;
+        mMaxValue = mMaxVal;
+        mDefaultValueExists = true;
+        mDefaultValue = mDefVal;
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+        mValue = Integer.parseInt(sharedPrefs.getString(HBMFragment.HBM_DISABLE_TIME_KEY, "1"));
+
+        setPersistent(false);
+    }
+
+    @Override
+    protected void changeValue(int newValue) {
+        PreferenceManager.getDefaultSharedPreferences(getContext()).edit()
+                .putString(HBMFragment.HBM_DISABLE_TIME_KEY, String.valueOf(newValue))
+                .apply();
+    }
+}
---
+++ parts/src/co/aospa/settings/thermal/TouchSettingsFragment.java
@@ -0,0 +1,149 @@
+/**
+ * Copyright (C) 2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package co.aospa.settings.thermal;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import android.view.MenuItem;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceManager;
+
+import com.android.settingslib.widget.MainSwitchPreference;
+import com.android.settingslib.widget.SettingsBasePreferenceFragment;
+
+import co.aospa.settings.R;
+import co.aospa.settings.widget.SeekBarPreference;
+
+public class TouchSettingsFragment extends SettingsBasePreferenceFragment
+        implements SharedPreferences.OnSharedPreferenceChangeListener,
+        Preference.OnPreferenceChangeListener {
+
+    private SharedPreferences mSharedPrefs;
+    private SeekBarPreference mTouchSensitivity;
+    private SeekBarPreference mTouchResponse;
+    private SeekBarPreference mTouchResistant;
+    private MainSwitchPreference mGameMode;
+
+    private String packageName = "";
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.touch_settings, rootKey);
+        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+
+        Bundle bundle = getArguments();
+        String appName = "";
+        if (bundle != null) {
+            appName = bundle.getString("appName", "");
+            packageName = bundle.getString("packageName", "");
+        }
+
+        getActivity().setTitle(getResources().getString(R.string.touch_control_title));
+
+        mGameMode = (MainSwitchPreference) findPreference(Constants.PREF_TOUCH_GAME_MODE);
+        if (mGameMode != null) {
+            mGameMode.setOnPreferenceChangeListener(this);
+        }
+
+        mTouchResistant = (SeekBarPreference) findPreference(Constants.PREF_TOUCH_RESISTANT);
+        mTouchResponse = (SeekBarPreference) findPreference(Constants.PREF_TOUCH_RESPONSE);
+        mTouchSensitivity = (SeekBarPreference) findPreference(Constants.PREF_TOUCH_SENSITIVITY);
+        updateDefaults();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mSharedPrefs.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mSharedPrefs.unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            getActivity().onBackPressed();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPrefs, String key) {
+        if (Constants.PREF_TOUCH_GAME_MODE.equals(key)) {
+            updateTouchModes(sharedPrefs.getBoolean(key, false) ? 1 : 0,
+                    Constants.TOUCH_GAME_MODE);
+        } else if (Constants.PREF_TOUCH_RESPONSE.equals(key)) {
+            updateTouchModes(sharedPrefs.getInt(key, 0), Constants.TOUCH_RESPONSE);
+        } else if (Constants.PREF_TOUCH_SENSITIVITY.equals(key)) {
+            updateTouchModes(sharedPrefs.getInt(key, 0), Constants.TOUCH_SENSITIVITY);
+        } else if (Constants.PREF_TOUCH_RESISTANT.equals(key)) {
+            updateTouchModes(sharedPrefs.getInt(key, 0), Constants.TOUCH_RESISTANT);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (Constants.PREF_TOUCH_GAME_MODE.equals(preference.getKey())) {
+            boolean isChecked = (Boolean) newValue;
+            mTouchSensitivity.setEnabled(isChecked);
+            mTouchResponse.setEnabled(isChecked);
+            mTouchResistant.setEnabled(isChecked);
+        }
+        return true;
+    }
+
+    private void updateDefaults() {
+        String[] values = getTouchValues().split(",");
+        boolean modeEnabled = Integer.parseInt(values[Constants.TOUCH_GAME_MODE]) == 1;
+        mGameMode.setChecked(modeEnabled);
+
+        mTouchSensitivity.setEnabled(modeEnabled);
+        mTouchResponse.setEnabled(modeEnabled);
+        mTouchResistant.setEnabled(modeEnabled);
+
+        mTouchResponse.setProgress(Integer.parseInt(values[Constants.TOUCH_RESPONSE]));
+        mTouchSensitivity.setProgress(Integer.parseInt(values[Constants.TOUCH_SENSITIVITY]));
+        mTouchResistant.setProgress(Integer.parseInt(values[Constants.TOUCH_RESISTANT]));
+    }
+
+    private void writeTouchValues(String modes) {
+        mSharedPrefs.edit().putString(packageName, modes).apply();
+    }
+
+    public String getTouchValues() {
+        String values = mSharedPrefs.getString(packageName, null);
+        if (values == null || values.isEmpty()) {
+            values = "0,0,0,0";
+        }
+        writeTouchValues(values);
+        return values;
+    }
+
+    public void updateTouchModes(int value, int mode) {
+        String[] values = getTouchValues().split(",");
+        values[mode] = String.valueOf(value);
+        String finalValues = values[Constants.TOUCH_GAME_MODE] + "," + values[Constants.TOUCH_RESPONSE] + ","
+                + values[Constants.TOUCH_SENSITIVITY] + "," + values[Constants.TOUCH_RESISTANT];
+        writeTouchValues(finalValues);
+    }
+}
